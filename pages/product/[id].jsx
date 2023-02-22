3e049cc5cc1fe8b6ab7b0937c74c32405a799b26
web10crown <99588906+web10crown@users.noreply.github.com>
2023-02-23 00:19:55 +0530

    Update [id].jsx

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -70,7 +70,7 @@ export default Product;
 
 export const getServerSideProps = async ({ params }) => {
 	const res = await axios.get(
-		`http://localhost:3000/api/products/${params.id}`
+		`https://burger-crown.vercel.app/api/products/${params.id}`
 	);
 	return {
 		props: {
