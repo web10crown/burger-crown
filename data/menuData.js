5951d4f7006974865db147e8320595e1a7a51f61
web10crown <99588906+web10crown@users.noreply.github.com>
2023-02-22 23:19:31 +0530

    Add files via upload

---
+++ data/menuData.js
@@ -0,0 +1,54 @@
+export const menuData = [
+	{
+		img: "m1.png",
+		desc: "Best of Combos",
+	},
+	{
+		img: "m2.png",
+		desc: "Friends and Family",
+	},
+	{
+		img: "m3.png",
+		desc: "Valentine Combos",
+	},
+	{
+		img: "m4.png",
+		desc: "whoper",
+	},
+	{
+		img: "m5.png",
+		desc: "Top Picks",
+	},
+	{
+		img: "m6.png",
+		desc: "Stunner Menu",
+	},
+	{
+		img: "m7.png",
+		desc: "Burgers",
+	},
+	{
+		img: "m8.png",
+		desc: "New King Collection",
+	},
+	{
+		img: "m9.png",
+		desc: "Sides",
+	},
+	{
+		img: "m10.png",
+		desc: "Beverages",
+	},
+	{
+		img: "m11.png",
+		desc: "Desserts",
+	},
+	{
+		img: "m12.png",
+		desc: "Chicken Wings",
+	},
+	{
+		img: "m13.png",
+		desc: "BK Cafe",
+	},
+];
