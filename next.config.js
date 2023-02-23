48819d7121c0b5cd602983680ddc9a2ba1749044
web10crown <99588906+web10crown@users.noreply.github.com>
2023-02-23 21:44:00 +0530

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -2,6 +2,21 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
+  async headers() {
+    const headers = [];
+
+    headers.push({
+      headers: [
+        {
+          key: "X-Robots-Tag",
+          value: "noindex",
+        },
+      ],
+      source: "/:path*",
+    });
+
+    return headers;
+  },
 };
 
 module.exports = nextConfig;
